78c15537d56908fde63031686e9f982a7306b7c8
mahirtaq <mahirtaq@umich.edu>
2018-10-27 17:01:37 -0400

    added test for exponent

--- test_rpn.py
+++ test_rpn.py
@@ -21,3 +21,7 @@ class TestBasics(unittest.TestCase):
     def test_toomany(self):
         with self.assertRaises(ValueError):
             result = rpn.calculate('1 2 3 +')
+
+    def test_epo(self):
+        result = rpn.calculate('2 3 ^')
+        self.assertEqual(8, result)
