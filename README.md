482c2bddcc7e7e72ff79e2f685247b36834c446c
Mahir Taqbeem <mahirtaq@umich.edu>
2018-10-28 17:34:30 -0400

    added badge and coverage reports

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.org/mahirtaq/c4cs-f18-wk8Advanced.svg?branch=master)](https://travis-ci.org/mahirtaq/c4cs-f18-wk8Advanced)
--- rpn.py
+++ rpn.py
@@ -20,6 +20,8 @@ def calculate(arg):
                 result = val2 / val1
             elif token == '^':
                 result = val2 ** val1
+            elif token == '%':
+                result = val2 % val1
             stack.append(result)
 
     if len(stack) > 1:
